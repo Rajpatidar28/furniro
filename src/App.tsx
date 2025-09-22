a13a20ab60d0d4580090dcb879ff3985cbbfe4bc
Rajpatidar28 <patidarraj762@gmail.com>
2025-09-22 19:53:08 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import Categories from './components/Categories'
+import Products from './components/Products'
+import InspirationSection from './components/InspirationSection'
+import HashtagSection from './components/HashtagSection'
+import Footer from './components/Footer'
+import "./App.css"
+import DaisyTest from './DaisyTest'
+
+export default function App() {
+  return (
+    <div >
+
+
+      <Header />
+      <HeroSection />
+      <Categories />
+      <Products />
+      <InspirationSection />
+      <HashtagSection />
+      <Footer />
+
+    </div>
+  )
+}
